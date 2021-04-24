77298c756a0fa547b7e21d85aa750ee38507a304
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-04-24 16:19:37 +0200

    New Commit due to git / github issue

---
+++ js/index.js
@@ -0,0 +1,58 @@
+// Variable
+
+let myName = 'Martin';
+console.log (myName);
+
+let firstName = "Martin";
+let lastName = "de Bes";
+
+console.log (firstName, lastName);
+
+// Cannot be a reserved keyword
+// Should be meaningfull
+// Cannot start with a number (1Martin)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// Constants
+
+const interestRate = 0.3;
+console.log (interestRate);
+
+// Types (Primitives)
+// Strings
+
+let Name1 = 'Martin'; // string literal
+let Age = 49; // number literal
+let isApproved = false; // Boolean literal
+let firstName2 = undefined; // Undefined 
+let selectedColor = null; // null (clear value)
+
+// Boleans
+
+console.log (4+4);
+console.log (Age * 3);
+
+
+// het had er volgens de opdracht zo uit moeten zien, maar ik heb het een beetje zelfstandig geintepreteerd:
+
+console.log("Hello Winc Academy");
+let Name = "Marjolein";
+console.log(Name);
+// Je kunt geen variabele aanmaken met de naam if.
+let sum = 3 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "3" + "4";
+console.log("sum with strings:", sumWithStrings);
+let subtraction = 10 - 3;
+console.log("subtraction:", subtraction);
+let multiplication = 1483 * 6342;
+console.log("multiplication:", multiplication);
+let division = 12 / 3;
+console.log("division:", division);
+let modulus = 12 % 9;
+console.log("modulus:", modulus);
+let age = 48;
+console.log(typeof age);
+let ageAsString = "48";
+console.log(typeof ageAsString);
